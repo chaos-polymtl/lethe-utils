1ebecb24b730547d9211391c555ec9306b87e335
Bruno Blais <blais.bruno@gmail.com>
2021-11-09 07:21:10 -0500

    Add small script to merge dat files

---
+++ script/merge_dat_files.sh
@@ -0,0 +1,21 @@
+PREFIX=$1
+START=$2
+FINISH=$3
+OUTPUT=$4
+
+echo "Merging files with PREFIX $PREFIX'_'$START to $PREFIX'_'$FINISH"
+
+for f in $(seq $START $FINISH); 
+do
+  FILE="$PREFIX$f.dat"
+  echo "Merging file : $FILE"
+  if [ $f -eq $START ]
+  then
+      tail -n +2 $FILE > $OUTPUT
+  else
+    tail -n +2 $FILE >> $OUTPUT
+  fi
+  
+  
+done
+
