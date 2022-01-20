24f8e05b3a4d821eb9fde04ab7537b6267bcfec2
tonielgeitani <tonielgeitani@gmail.com>
2022-01-20 15:43:12 -0500

    add case1 MMs for model A

---
+++ cases/vans/case1/model_A/mmsVans_2d/mmsVans_2d.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+from sympy import *
+
+
+x, y, z, pi, mu, t = symbols('x y z pi mu t')
+
+def laplacian(f):
+    d2dx=diff(f,x,x)
+    d2dy=diff(f,y,y)
+
+    return d2dx+d2dy
+
+def conv(u,v,f):
+    cx = u * diff(f,x) 
+    cy = v * diff(f,y) 
+
+    return cx+cy
+
+def dx(f):
+    return diff(f,x)
+
+def dy(f):
+    return diff(f,y)
+
+eps = 0.5 + 0.25*sin(pi*x)*sin(pi*y)
+u = -2*sin(pi*x) * sin(pi*x) * cos(pi*y) * sin(pi*y) 
+v = 2*cos(pi*x) * sin(pi*x) * sin(pi*y) * sin(pi*y)
+
+p = sin(pi*x)*sin(pi*y)
+
+print ("Verifying divergence free")
+print (simplify(eps*(dx(u)+dy(v)) + u*(dx(eps)) + v*(dy(eps))))
+
+
+print ("Stokes X Source term:")
+print ((simplify(-laplacian(u)+dx(p))/eps))
+
+print ("Stokes Y Source term:")
+print ((simplify(-laplacian(v)+dy(p))/eps))
+
+print ("Convection X source term:")
+print ((simplify(conv(eps*u,eps*v,u))/eps))
+
+print ("Convection Y source term:")
+print ((simplify(conv(eps*u,eps*v,v))/eps))
+
+print("Total X")
+print (simplify((-eps*laplacian(u)+ eps*dx(p) + conv(eps*u,eps*v,u))/eps))
+print("Total Y")
+print (simplify((-eps*laplacian(v)+ eps*dy(p) + conv(eps*u,eps*v,v))/eps))
+
