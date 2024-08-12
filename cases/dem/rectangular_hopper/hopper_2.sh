54d7a5464f288d87ceeb5cea509de26d8315e6c5
Bruno Blais <blais.bruno@gmail.com>
2024-08-12 12:59:42 -0600

    Update launch files

--- cases/dem/rectangular_hopper/hopper_2.sh
+++ cases/dem/rectangular_hopper/hopper_2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --account=def-blaisbru
+#SBATCH --account=def-bertrand
 #SBATCH --ntasks-per-node=40
 #SBATCH --cpus-per-task=1
 #SBATCH --nodes=1 #number of whole nodes used (each with up to 40 tasks-per-node)
-#SBATCH --time=23:59:00 #maximum time for the simulation (hh:mm:ss)
+#SBATCH --time=10:59:00 #maximum time for the simulation (hh:mm:ss)
 #SBATCH --mem=128G
 #SBATCH --job-name=hopper_2
 #SBATCH --output=hopper_2.out
