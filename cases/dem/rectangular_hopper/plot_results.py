ee91810deb1d734ede6b665e11cab624a77b8e66
OGaboriault <oliviergabo@gmail.com>
2024-11-13 11:46:36 -0500

    rectangular hopper typo

--- cases/dem/rectangular_hopper/plot_results.py
+++ cases/dem/rectangular_hopper/plot_results.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 
 colors = ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e', '#e6ab02']
 
-files = ["results_2023-08.dat", "results-2023-12.dat", "results_2024-02.dat",
-        "results_2024-10_master.dat", "results_2024-10_test_branch.dat"]
-labels = ["08/2023", "12/2023-Ankerl", "02/2024", "master", "test branch"]
+files = ["results_2024-10_master.dat","results_2024-10_master_10000_lb.dat", "results_2024-10_master_10000_lb_1000_pw.dat"]
+labels = ["master", "LB: 10 000", "LB: 10 000 + PW: 1000" ]
 markers = ["o", "^", "s", "<", ">"]
 
 for i, f in enumerate(files):
     x, t = np.loadtxt(f, skiprows=1, unpack=True)
+    print(t)
     plt.plot(x, t / 60, '--', marker=markers[i], label=labels[i],
              color=colors[i])
 
