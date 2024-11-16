694705de1ec5ef989c09e390fbd35ee371639654
OGaboriault <oliviergabo@gmail.com>
2024-11-16 16:25:53 -0500

    Changed number of core

---
+++ cases/dem/rectangular_hopper/hopper_160.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --account=def-bertrand
+#SBATCH --ntasks-per-node=40
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=1 #number of whole nodes used (each with up to 40 tasks-per-node)
+#SBATCH --time=10:59:00 #maximum time for the simulation (hh:mm:ss)
+#SBATCH --job-name=hopper_160
+#SBATCH --output=hopper_160.out
+
+export OMP_NUM_THREADS=1
+source $HOME/.dealii
+time mpirun -np 160 lethe-particles hopper_160.prm
