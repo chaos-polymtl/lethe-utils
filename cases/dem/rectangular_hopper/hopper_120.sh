a1f1f7a8cd1af0965fca5ef8f5c97ed46d6ed260
OGaboriault <oliviergabo@gmail.com>
2024-11-16 16:28:06 -0500

    Number of nodes

--- cases/dem/rectangular_hopper/hopper_120.sh
+++ cases/dem/rectangular_hopper/hopper_120.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-bertrand
 #SBATCH --ntasks-per-node=40
 #SBATCH --cpus-per-task=1
-#SBATCH --nodes=1 #number of whole nodes used (each with up to 40 tasks-per-node)
+#SBATCH --nodes=3 #number of whole nodes used (each with up to 40 tasks-per-node)
 #SBATCH --time=10:59:00 #maximum time for the simulation (hh:mm:ss)
 #SBATCH --job-name=hopper_120
 #SBATCH --output=hopper_120.out
--- cases/dem/rectangular_hopper/hopper_160.sh
+++ cases/dem/rectangular_hopper/hopper_160.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=def-bertrand
 #SBATCH --ntasks-per-node=40
 #SBATCH --cpus-per-task=1
-#SBATCH --nodes=1 #number of whole nodes used (each with up to 40 tasks-per-node)
+#SBATCH --nodes=4 #number of whole nodes used (each with up to 40 tasks-per-node)
 #SBATCH --time=10:59:00 #maximum time for the simulation (hh:mm:ss)
 #SBATCH --job-name=hopper_160
 #SBATCH --output=hopper_160.out
