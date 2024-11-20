ca94091faf3a76b3a2a12428b38b78d724a729b8
OGaboriault <oliviergabo@gmail.com>
2024-11-20 15:08:37 -0500

    Results and meshes

--- cases/dem/rectangular_hopper/plot_results.py
+++ cases/dem/rectangular_hopper/plot_results.py
@@ -1,20 +1,64 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
+
 colors = ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e', '#e6ab02']
 
-files = ["results_2024-10_master.dat","results_2024-10_master_10000_lb.dat", "results_2024-10_master_10000_lb_1000_pw.dat"]
-labels = ["master", "LB: 10 000", "LB: 10 000 + PW: 1000" ]
+from cycler import cycler
+
+colors=['#1b9e77','#d95f02','#7570b3','#e7298a','#66a61e','#e6ab02']
+
+plt.rcParams['axes.prop_cycle'] = cycler(color = colors)
+plt.rcParams['figure.facecolor'] = 'white'
+plt.rcParams['figure.figsize'] = (10,8)
+plt.rcParams['lines.linewidth'] = 4
+plt.rcParams['lines.markersize'] = '11'
+plt.rcParams['markers.fillstyle'] = "none"
+plt.rcParams['lines.markeredgewidth'] = 2
+plt.rcParams['legend.columnspacing'] = 2
+plt.rcParams['legend.handlelength'] = 3
+plt.rcParams['legend.handletextpad'] = 0.2
+plt.rcParams['legend.frameon'] = True
+plt.rcParams['legend.fancybox'] = False
+plt.rcParams['xtick.major.width'] = 2
+plt.rcParams['xtick.major.size'] = 5
+plt.rcParams['ytick.major.size'] = 5
+plt.rcParams['ytick.major.width'] = 2
+plt.rcParams['font.size'] = '25'
+plt.rcParams['font.family']='DejaVu Serif'
+plt.rcParams['font.serif']='cm'
+plt.rcParams['savefig.bbox']='tight'
+plt.rcParams['legend.handlelength']=1
+
+files = ["results_2024-11_master_2.dat"]
+labels = ["LB: 10_000 + PW: 10_000","LB: 10_000 + PW: 2_000" ]
 markers = ["o", "^", "s", "<", ">"]
+
+
+# Create the plot
+fig, ax1 = plt.subplots()
+ax2 = ax1.twiny()
 
 for i, f in enumerate(files):
     x, t = np.loadtxt(f, skiprows=1, unpack=True)
     print(t)
-    plt.plot(x, t / 60, '--', marker=markers[i], label=labels[i],
+    # Plot the primary y-axis
+    ax1.plot(x, t / 60, '--', marker=markers[i], label=labels[i],
              color=colors[i])
 
-plt.xlabel("number of cores")
-plt.ylabel("Simulation time [min]")
-plt.legend()
-plt.savefig("results.png", dpi=300)
+# Customize the first y-axis
+ax1.set_xlabel("Number of cores")
+ax1.set_ylabel("Simulation time [min]")
+ax1.set_ylim([0, 650])
+
+new_tick_location = np.array([40,80,120,160,200])
+
+ax2.set_xlim(ax1.get_xlim())
+ax2.set_xticks(new_tick_location)
+ax2.set_xticklabels(np.array(new_tick_location/40,dtype=int))
+ax2.set_xlabel("Number of nodes")
+
+#plt.savefig("results.png", dpi=300)
 plt.show()
