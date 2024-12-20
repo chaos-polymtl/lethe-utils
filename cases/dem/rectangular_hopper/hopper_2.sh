b22c3cb09ee27bbf30960b34e8f536a18c37f9e0
Bruno Blais <blais.bruno@gmail.com>
2024-12-20 15:02:26 -0500

    Update the .sh file to have memory specifications

--- cases/dem/rectangular_hopper/hopper_2.sh
+++ cases/dem/rectangular_hopper/hopper_2.sh
@@ -2,6 +2,7 @@
 #SBATCH --account=def-bertrand
 #SBATCH --ntasks-per-node=40
 #SBATCH --cpus-per-task=1
+#SBATCH --mem=80G
 #SBATCH --nodes=1 #number of whole nodes used (each with up to 40 tasks-per-node)
 #SBATCH --time=10:59:00 #maximum time for the simulation (hh:mm:ss)
 #SBATCH --job-name=hopper_2
