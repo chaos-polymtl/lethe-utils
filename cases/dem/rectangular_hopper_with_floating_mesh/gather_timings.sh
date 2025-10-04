4eb1078d8d3fed159db315706d316f422f3d366a
Bruno Blais <blais.bruno@gmail.com>
2025-10-04 14:21:54 +0200

    Update timing gathering

--- cases/dem/rectangular_hopper_with_floating_mesh/gather_timings.sh
+++ cases/dem/rectangular_hopper_with_floating_mesh/gather_timings.sh
@@ -1,1 +1,1 @@
-for i in $(ls *.out); echo $i ; grep "Total wall" $i | tail -1 | cut -c 50- | cut -c -10; done
+for i in $(ls *.out); do echo $i ; grep "Total wall" $i | tail -1 | cut -c 50- | cut -c -10; done
