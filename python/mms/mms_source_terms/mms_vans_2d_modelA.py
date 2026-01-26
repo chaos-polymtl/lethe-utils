0ecdc75ad18c4e1197ea502cd689cd198e3846f3
antonellasuccar <antonella.succar@polymtl.ca>
2026-01-25 20:58:09 -0500

    Add missing terms in python script and add prm file

--- python/mms/mms_source_terms/mms_vans_2d_modelA.py
+++ python/mms/mms_source_terms/mms_vans_2d_modelA.py
@@ -2,20 +2,47 @@
 
 from sympy import *
 
-# Cases currently supported by this mms generator are case0, case1 and case2
+# Cases currently supported by this mms generator are case0, case1 and case2. This code assumes a kinematic viscosity
+# of 1.
 case="case2"
 
 
 # Define symbolic variables that are used to generate the MMS
 x, y, z, pi, mu, t = symbols('x y z pi mu t')
 
+# Differentiate with respect to x
+def dx(f):
+    return diff(f,x)
+
+# Differentiate with respect to y
+def dy(f):
+    return diff(f,y)
+
+# Differentiate with respect to time
+def dt(f):
+    return diff(f,t) 
 
 # Laplacian operator
 def laplacian(f):
     d2dx=diff(f,x,x)
     d2dy=diff(f,y,y)
-
+    
     return d2dx+d2dy
+    
+# Divergence operator
+def div(u, v):
+    
+    return dx(u) + dy(v)
+    
+# Gradient of the divergence of a vector operator
+def div_grad(u,v,component):
+    cx = diff(div(u,v),x)
+    cy = diff(div(u,v),y) 
+
+    if (component==1):
+        return cx
+    else:
+        return cy    
 
 # Time derivative operator
 def dtt(g,u,v):
@@ -30,19 +57,7 @@ def conv(u,v,f):
     cx = u * diff(f,x) 
     cy = v * diff(f,y) 
 
-    return cx+cy
-
-# Differentiate with respect to x
-def dx(f):
-    return diff(f,x)
-
-# Differentaite with respect to y
-def dy(f):
-    return diff(f,y)
-
-# Differentiate with respect ot time
-def dt(f):
-    return diff(f,t)  
+    return cx+cy 
     
 
 # Three MMS cases that are defined for the VANS equations
@@ -76,10 +91,10 @@ print ("Unsteady Y Source term:")
 print (simplify(eps*dt(v)))
 
 print ("Stokes X Source term:")
-print (simplify((-laplacian(u)+dx(p))/eps))
+print (simplify((-laplacian(u)-div_grad(u,v,1)+dx(p))/eps))
 
 print ("Stokes Y Source term:")
-print (simplify((-laplacian(v)+dy(p))/eps))
+print (simplify((-laplacian(v)-div_grad(u,v,2)+dy(p))/eps))
 
 print ("Convection X source term:")
 print (simplify((conv(eps*u,eps*v,u))/eps))
@@ -90,6 +105,7 @@ print (simplify((conv(eps*u,eps*v,v))/eps))
 
 # Total source term. Be careful, to implement in a muparser syntax, the ** need to be replaced by ^^^^
 print("Total ")
+print("original ")
 print (simplify((eps*dt(u)+u*(dt(eps) + eps*(dx(u)+dy(v)) + u*(dx(eps)) + v*(dy(eps))) - eps*laplacian(u) - 0*dx(eps)*dx(u)-0*dy(eps)*dy(u)+eps*dx(p) + conv(eps*u,eps*v,u))/eps), " ; ", simplify((eps*dt(v)+v*(dt(eps) + eps*(dx(u)+dy(v)) + u*(dx(eps)) + v*(dy(eps))) - eps*laplacian(v) - 0*dx(eps)*dx(v)-0*dy(eps)*dy(v)+eps*dy(p) + conv(eps*u,eps*v,v))/eps), " ; ", "0" )
-
-
+print("with missing term")
+print (simplify((eps*dt(u)+u*(dt(eps) + eps*div(u,v) + u*(dx(eps)) + v*(dy(eps))) - eps*laplacian(u) - eps*div_grad(u,v,1) - 0*dx(eps)*dx(u)-0*dy(eps)*dy(u)+eps*dx(p) + conv(eps*u,eps*v,u))/eps), " ; ", simplify((eps*dt(v)+v*(dt(eps) + eps*div(u,v) + u*(dx(eps)) + v*(dy(eps))) - eps*laplacian(v) - eps*div_grad(u,v,2) - 0*dx(eps)*dx(v)-0*dy(eps)*dy(v)+eps*dy(p) + conv(eps*u,eps*v,v))/eps), " ; ", "0" )
